0210a6904644fff38d3d637f1538d8959f147f13
SHUBHAM BISWAS <suvm.dev@gmail.com>
2024-01-20 17:20:09 +0530

    Initial Commit

---
+++ SwiftUIChatApplication/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIChatApplication
+//
+//  Created by Shubham Biswas on 20/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIChatApplication/SwiftUIChatApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChatApplicationApp.swift
+//  SwiftUIChatApplication
+//
+//  Created by Shubham Biswas on 20/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChatApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
